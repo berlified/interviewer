e8aa12e265964d3d4e9420f70ac2af8ec816ba35
Niranjan R <niranjanr753@gmail.com>
2026-06-25 12:42:42 +0530

    added router and loading states

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { PreInterviewBody } from "./types";
 import { scrapeGithub } from "./scrapers/github";
 import cors from "cors";
+import {prisma} from "./db";
 
 const app = express();
 app.use(express.json());
@@ -19,12 +20,17 @@ app.post("/api/v1/pre-interview" , async (req, res) => {
     }
 
     const githubUrl = data.github.endsWith("/") ? data.github.slice(0,-1) : data.github;
-
     const githubUsername = githubUrl.split("/").pop()!;
-
     const githubData = await scrapeGithub(githubUsername);
 
-    res.json({github :githubData});
+    const interview = await prisma.interview.create({
+        data: {
+            githubMetaData: JSON.stringify(githubData),
+            status: "Pre"
+        }
+    })
+
+    res.json({id :interview.id});
 })
 
 app.listen(3001, () => {
